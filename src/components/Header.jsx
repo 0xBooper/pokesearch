37c84404df4c4787f34641d454e357f799fd48f1
0xBooper <77999320+0xBooper@users.noreply.github.com>
2021-08-09 22:51:44 -0500

    feat: Split the header into it's own component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Pokesearch</h1>
+            <p>
+                Where you can search for pokemon. <br/> Below is a search bar
+                where you can search using the name of the pokemon.
+            </p>
+        </div>
+    )
+}
+
+export default Header
