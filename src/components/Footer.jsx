666ed4cf6666cbb2c084567c02c48744b0e11ae1
0xBooper <77999320+0xBooper@users.noreply.github.com>
2021-08-13 22:50:59 -0500

    feat: Finish the footer and it's components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+
+// ? Import Font Awesome icons
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// ? CSS imports
+import "../styles/Footer.css"
 
 const Footer = () => {
     return (
         <div className="footer">
-            <p>Made by <a href="https://github.com/0xBooper" target="_blank">0xBooper</a>.</p>
+            <p>Made by <a href="https://0xBooper.github.io" target="_blank">0xBooper | epikbooperia</a>.</p>
             <a href="https://github.com/0xBooper/pokesearch" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faGithub} size="lg" />
             </a>
         </div>
     )
